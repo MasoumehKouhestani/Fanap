f5ee6b354cc32e907ff035f4814f2d91d3987c71
m.koohestani <angelmoonstar77@gmail.com>
2023-12-31 20:14:26 +0330

    Change project format to maven project

--- src/main/java/ir/fanap/basecodes/Main.java
+++ src/main/java/ir/fanap/basecodes/Main.java
@@ -1,3 +1,5 @@
+package ir.fanap.basecodes;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
--- src/main/java/ir/fanap/basecodes/RecursiveFunctions.java
+++ src/main/java/ir/fanap/basecodes/RecursiveFunctions.java
@@ -1,3 +1,5 @@
+package ir.fanap.basecodes;
+
 public class RecursiveFunctions {
     public static void main(String[] args) {
         int n = 4;
